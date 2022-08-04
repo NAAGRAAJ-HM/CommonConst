c71bcde196bb0f8e90708db99c142d2bc37c463f
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-08-04 19:53:46 +0200

    Update: Project level constants integration files

---
+++ main_LATEST/ConstServiceDcm.hpp
@@ -0,0 +1,71 @@
+#pragma once
+/******************************************************************************/
+/* File   : ConstServiceDcm.hpp                                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+#include "ConstModule.hpp"
+
+#include "infServiceDcmClient.hpp"
+
+#if(STD_ON == _ReSIM)
+#include <iostream>
+using namespace std;
+#else
+#endif
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTDCM_AR_RELEASE_VERSION_MAJOR                                      4
+#define CONSTDCM_AR_RELEASE_VERSION_MINOR                                      3
+#define ServiceDcm_SizeinfServiceDcmClients                                                 59
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+#if(CONSTDCM_AR_RELEASE_VERSION_MAJOR != STD_AR_RELEASE_VERSION_MAJOR)
+   #error "Incompatible CONSTDCM_AR_RELEASE_VERSION_MAJOR!"
+#endif
+
+#if(CONSTDCM_AR_RELEASE_VERSION_MINOR != STD_AR_RELEASE_VERSION_MINOR)
+   #error "Incompatible CONSTDCM_AR_RELEASE_VERSION_MINOR!"
+#endif
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class ConstServiceDcm_Type:
+      public ConstModule_TypeAbstract
+{
+   public:
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+      uint8         u8SizeinfServiceDcmClients;
+      infServiceDcmClient* aptrServiceDcmClients[ServiceDcm_SizeinfServiceDcmClients];
+#if(STD_ON == _ReSIM)
+      string        astrServiceDcmClientNames[ServiceDcm_SizeinfServiceDcmClients];
+#else
+#endif
+};
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
